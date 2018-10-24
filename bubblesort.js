e9c27e43add5bd459ea6161eedf709abc512efef
Elemino <elemshalit@gmail.com>
2018-10-23 22:10:46 -0700

    Add bubble sort first step

---
+++ bubblesort.js
@@ -0,0 +1,13 @@
+items = [9,2,5,6,4,3,7,10,1,8]
+
+function bubblesort (item) {
+    let arrayLength = items.length;
+    
+    for (let firstLoopPosition = 0; firstLoopPosition < arrayLength; firstLoopPosition++) {
+        console.log( "outer count: " + firstLoopPosition)
+    
+     for (let secondLoopPosition = 0; secondLoopPosition < (arrayLength - firstLoopPosition - 1); secondLoopPosition++) {
+         console.log("inner loop count: " + secondLoopPosition + " going up to index " + (arrayLength - firstLoopPosition - 1))
+     }
+   }
+}
